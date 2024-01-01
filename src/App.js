f97795321923cafbe1e8655028acedf934a2beb0
Riwalenn Bas <riwalenn@gmail.com>
2024-01-01 18:43:43 +0100

    Building Components & css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import ParticlesBg from 'particles-bg';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ParticlesBg color="#5c5c5c" num={100} type="cobweb" bg={true}/>
+            <Navigation/>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm/>
+            {/*<FaceRecognition />*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div className='center' style={{alignItems: "center"}}>
+            <div className='f3 pa3'>
+                {'Riwalenn, your current rank is...'}
+            </div>
+            <div className='f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
