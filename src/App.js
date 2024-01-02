314381816e96f4d459b4ee8436721c30776b288e
Riwalenn Bas <riwalenn@gmail.com>
2024-01-02 17:24:34 +0100

    Face Detection Box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import {Component} from "react";
 import ParticlesBg from 'particles-bg';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import {Component} from "react";
-import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import './App.css';
 
 const returnCarifaiRequestOptions = (imageUrl) => {
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
@@ -50,7 +50,25 @@ class App extends Component {
         this.state = {
             input: '',
             imageUrl: '',
+            box: {}
         }
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        };
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box});
     }
 
     onInputChange = (event) => {
@@ -64,24 +82,10 @@ class App extends Component {
         // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
         // this will default to the latest version_id
         // eslint-disable-next-line no-useless-concat
-        fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnCarifaiRequestOptions(this.state.input))
+        fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnCarifaiRequestOptions(this.state.input))
             .then(response => response.json())
-            .then(response => {
-                console.log('hi', response.outputs[0].data.regions[0].region_info.bounding_box)
-                // if (response) {
-                //     fetch('http://localhost:3000/image', {
-                //         method: 'put',
-                //         headers: {'Content-Type': 'application/json'},
-                //         body: JSON.stringify({
-                //             id: this.state.user.id
-                //         })
-                //     })
-                //         .then(response => response.json())
-                //         .then(count => {
-                //
-                //         })
-                // }
-            })
+            .then(response => this.calculateFaceLocation(response))
+            .catch(err => console.error(err));
     }
 
     render() {
@@ -96,7 +100,7 @@ class App extends Component {
                     onInputChange={this.onInputChange}
                     onButtonSubmit={this.onButtonSubmit}
                 />
-                <FaceRecognition imageUrl={imageUrl} />
+                <FaceRecognition box={this.state.box} imageUrl={imageUrl} />
             </div>
         )
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({box, imageUrl}) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputimage' alt='' src={imageUrl} style={{ width: '500px', height: 'auto', marginTop: '10px' }} />
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             </div>
         </div>
     );
