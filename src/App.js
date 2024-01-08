f9cf08a0640c89a2583d5371a2409577ab19cbf2
Riwalenn Bas <riwalenn@gmail.com>
2024-01-08 19:36:21 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -46,33 +46,34 @@ const returnCarifaiRequestOptions = (imageUrl) => {
     };
 };
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+}
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState
     }
 
-    loadUser = (user) => {
-        this.setState({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            entries: user.entries,
-            joined: user.joined
-        })
+    loadUser = (data) => {
+        this.setState({user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined
+            }})
     }
 
     componentDidMount() {
@@ -119,7 +120,8 @@ class App extends Component {
                         .then(response => response.json())
                         .then(count => {
                             this.setState(Object.assign(this.state.user, { entries: count }))
-                        });
+                        })
+                        .catch(err => console.error(err));
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -128,7 +130,7 @@ class App extends Component {
 
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({isSignedIn: false});
+            this.setState(initialState);
         } else if(route === 'home') {
             this.setState({isSignedIn: true});
         }
